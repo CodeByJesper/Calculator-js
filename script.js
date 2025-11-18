533fff8f77afab751be7eaedb5ea385379e4e4d9
BlueTeam-Jesper <lyppert@hotmail.se>
2025-11-18 17:34:22 +0100

    Add number input functionality
    Added event listeners to all buttons and implemented logic to update display when a numeric button is pressed.

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 // My script
+
+// Declare variables for display and buttons
+const display = document.getElementById("displayText");
+const buttons = document.querySelectorAll("button")
+
+// Add eventlistners to all buttons
+buttons.forEach(function(button){
+    button.addEventListener('click', function(e){
+        // Declare value to hold clicked buttons value
+        const value = e.target.textContent;
+        // If value of button is an integer add number to display
+       if(isFinite(value)){
+        console.log('Nummer')
+        display.innerHTML += value
+       }
+    })
+})
+
